8d6ec7d5b09129f11acc77b01a921ee67a7ec85c
Helder <helder@lamoulade.com>
2016-01-05 15:13:21 +0000

    initialised the animation on scroll

--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -3,27 +3,30 @@ console.log('navbarjs loaded!');
 (function () {
 
 	var header = document.querySelector('header'),
-		header_height      = getComputedStyle(header).height.split('px')[0],
+		header_height = getComputedStyle(header).height.split('px')[0],
 		fixedClassName = 'minimize',
 		headerLogo = document.querySelector('.header-logo');
 
 	function stickyScroll(e) {
 		if( window.pageYOffset > (header_height) / 2 ) {
-			TweenLite.to(headerLogo, 0.3, { scale: 0, onComplete: function () {
-					header.classList.add(fixedClassName);
-				}
-			});
+
+			var tl = new TimelineLite();
+			tl.to(headerLogo, 0.3, { scale: 0 });
+			tl.set(header, { className: '+=' + fixedClassName });
+
 		}
 
 		if( window.pageYOffset < (header_height) / 2 ) {
-			TweenLite.to(headerLogo, 0.3, { scale: 1, onComplete: function () {
-					header.classList.remove(fixedClassName);
-				}
-			});
+
+			var tl = new TimelineLite();
+
+			tl.to(headerLogo, 0.3, { scale: 1 });
+			tl.set(header, { className: '-=' + fixedClassName });
+
 		}
 	}
 
 	// Scroll handler to toggle classes.
-	window.addEventListener('scroll', _.throttle(stickyScroll, 100), false);
+	window.addEventListener('scroll', _.throttle(stickyScroll, 200), false);
 
 }());
