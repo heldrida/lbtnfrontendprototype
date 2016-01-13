b087bd0dde0e71266e29b4ed55f16a92146386e0
Helder <helder@lamoulade.com>
2016-01-13 15:24:26 +0000

    renamed the main menu drop down file name; created the initial script to handle the menu option click actions to expand the drop down menu;

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -95,12 +95,44 @@ console.log('%c mainMenu.js loaded!', 'background: #0C0; padding: 2px; color: #F
 
 	function MainMenu (params) {
 		this.init(params);
+		this.setEventListeners();
 	}
 
 	MainMenu.prototype = {
+
 		init: function (params) {
+			this.header = document.querySelector('header');
+			this.mainMenu = document.querySelector('.main-menu');
+			this.mainMenuOptList = document.querySelectorAll('li');
+			this.layerBottom = document.querySelector('header .layer-bottom');
+			this.menuDropDownContainer = document.querySelector('.menu-drop-down-container');
+		},
+
+		setEventListeners: function () {
+
+			for (var i = 0; i < this.mainMenuOptList.length; i++) {
+				this.mainMenuOptList[i].addEventListener('click', this.menuOptionHandler.bind(this));
+			}
+
+		},
+
+		menuOptionHandler: function (e) {
+
+			this.menuDropDownContainer
+
+			var tl = new TimelineLite({
+				onStart: function () {
+					this.header.classList.add('drop-down-menu-open');
+				}.bind(this),
+				onReverseComplete: function () {
+
+				}.bind(this)
+			});
+
+			tl.to(this.layerBottom, 0.3, { css: { y: 0 } });
 
 		}
+
 	};
 
 	module.exports = MainMenu;
